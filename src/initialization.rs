53f8aabfb98b429b545ed3469d76cfb41936ea86
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2024-12-31 10:13:19 +0100

    feat(initialization): sprinkle logs

--- src/initialization.rs
+++ src/initialization.rs
@@ -2,6 +2,7 @@ use sdl2::image::InitFlag;
 use sdl2::render::Canvas;
 use sdl2::video::Window;
 use sdl2::Sdl;
+use tracing::debug;
 
 pub fn setup_sdl() -> (
     Sdl,
@@ -9,6 +10,7 @@ pub fn setup_sdl() -> (
     sdl2::render::TextureCreator<sdl2::video::WindowContext>,
 ) {
     let sdl_context = sdl2::init().unwrap();
+    debug!("sdl initialized");
     let video_subsystem = sdl_context.video().unwrap();
     let _image_context = sdl2::image::init(InitFlag::PNG).unwrap();
 
@@ -17,6 +19,7 @@ pub fn setup_sdl() -> (
         .position_centered()
         .build()
         .unwrap();
+    debug!("window created");
 
     let canvas = window.into_canvas().software().build().unwrap();
     let texture_creator = canvas.texture_creator();
