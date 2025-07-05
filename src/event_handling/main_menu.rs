90cc7d7a5c7210874a3c079d4d68459b94a566b5
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2025-07-05 16:36:19 +0200

    refactor(main_menu): formatting

--- src/event_handling/main_menu.rs
+++ src/event_handling/main_menu.rs
@@ -8,16 +8,19 @@ pub fn handle_main_menu_input(
     state_guard: &mut MutexGuard<GameState>,
 ) -> Option<Signal> {
     if let Event::KeyDown {
-            keycode: Some(keycode),
-            ..
-        } = event { match *keycode {
-        Keycode::Return | Keycode::Space | Keycode::P => {
-            **state_guard = GameState::Playing;
+        keycode: Some(keycode),
+        ..
+    } = event
+    {
+        match *keycode {
+            Keycode::Return | Keycode::Space | Keycode::P => {
+                **state_guard = GameState::Playing;
+            }
+            Keycode::Q => {
+                return Some(Signal::Quit);
+            }
+            _ => {}
         }
-        Keycode::Q => {
-            return Some(Signal::Quit);
-        }
-        _ => {}
-    } }
+    }
     None
 }
